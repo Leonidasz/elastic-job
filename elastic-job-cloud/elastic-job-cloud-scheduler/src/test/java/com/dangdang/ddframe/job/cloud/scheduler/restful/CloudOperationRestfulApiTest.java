751e6319bb8441df5536e08a82e9d3522dd5e6a8
gaohongtao <gaohongtao@dangdang.com>
2017-07-18 15:59:57 +0800

    Add media type

--- elastic-job-cloud/elastic-job-cloud-scheduler/src/test/java/com/dangdang/ddframe/job/cloud/scheduler/restful/CloudOperationRestfulApiTest.java
+++ elastic-job-cloud/elastic-job-cloud-scheduler/src/test/java/com/dangdang/ddframe/job/cloud/scheduler/restful/CloudOperationRestfulApiTest.java
@@ -57,4 +57,13 @@ public class CloudOperationRestfulApiTest extends AbstractCloudRestfulApiTest {
         when(getRegCenter().getDirectly(HANode.FRAMEWORK_ID_NODE)).thenReturn("not-exists");
         assertThat(sentGetRequest("http://127.0.0.1:19000/api/operate/sandbox?appName=foo_app"), is("[]"));
     }
+    
+    @Test
+    public void assertTaskSandbox() throws Exception {
+        when(getRegCenter().getDirectly(HANode.FRAMEWORK_ID_NODE)).thenReturn("d8701508-41b7-471e-9b32-61cf824a660d-0000");
+        assertThat(sentGetRequest("http://127.0.0.1:19000/api/operate/task/sandbox/"
+                + "cpu_job_1@-@1@-@READY@-@d8701508-41b7-471e-9b32-61cf824a660d-S0@-@ede114f0-f2db-4bad-b0e0-e820a7d19c59"), is("127.0.0.1:9050/#/"
+                + "agents/d8701508-41b7-471e-9b32-61cf824a660d-S0/browse?path=/home/gaohongtao/mesos/work-1.1.0/slaves/d8701508-41b7-471e-9b32-61cf824a660d-S0"
+                + "/frameworks/d8701508-41b7-471e-9b32-61cf824a660d-0000/executors/foo_app@-@d8701508-41b7-471e-9b32-61cf824a660d-S0/runs/53fb4af7-aee2-44f6-9e47-6f418d9f27e1"));
+    }
 }
